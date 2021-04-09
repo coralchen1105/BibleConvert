87c0efb228c4ddea1f839449f96cdd2cfdf9f232
coralchen1105 <coralchen1105@gmail.com>
2021-04-09 11:43:30 +1000

    Update main.py
    make every script as an element in list

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ lines = text.split('\n')
 title = lines[0]
 
 # create subString list to remove the lines contain subString
-subString = ["Chapter", "("]
+subString = ["^Chapter", "\(\d+"]
 
 outlines = []
 
 # outlines and chapter lines into list
 for line in lines:
     for sub in subString:
-        if line.find(sub) != -1:
+        if re.search(sub, line):
             outlines.append(line)
 
 # get the different elements from two list (lines, outlines)
@@ -24,16 +24,17 @@ cleanContent = content[2:]
 
 print(cleanContent)
 
-# test to find "\xa0"
-for e in cleanContent:
-    x = re.search("\xa0", e)
-    if x:
-        print("found")
-
 # join the whole list (remove outline, title, and chapter) into one string
 str1: str = ''.join(cleanContent)
 
+
 # to-do: use regex to split all scripts into a list, means each script(with script number) will be an element
-y = re.search("\xa0", str1)
+y = re.split("\d+\xa0", str1)
+i = 0
+for e in y:
+    i = i+1
+    print(str(i) + e)
+
+
 
 
